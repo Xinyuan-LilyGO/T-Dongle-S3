78e4eee50852651567b4508c09c7ac39032b3456
lewisxhe <lewishe@outlook.com>
2025-12-19 11:27:29 +0800

    Update fw

--- firmware/flash to device.bat
+++ firmware/flash to device.bat
@@ -1,5 +1,5 @@
 :start
-esptool.exe --chip esp32s3   --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 firmware.bin
+esptool.exe --chip esp32s3   --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 T-Dongle-S3_factory_screen_251218.bin
 
 @echo Press any key,keey find device praogram
 pause
