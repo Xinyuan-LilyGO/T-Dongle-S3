1d3f0e5709e2af6bf2b30ca689cfbdf03e65cbec
lewisxhe <lewishe@outlook.com>
2026-04-03 10:59:11 +0800

    Update docs

--- firmware/Flash T-Dongle-S3.bat
+++ firmware/Flash T-Dongle-S3.bat
@@ -1,7 +1,7 @@
 :start
 esptool.exe --chip esp32s3   --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 T-Dongle-S3_factory_screen_251218.bin
 
-@echo Press any key,keey find device praogram
+@echo Press any key, keep finding device program
 pause
 goto start
 
